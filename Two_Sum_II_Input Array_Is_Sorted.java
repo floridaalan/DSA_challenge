bd0c02a478e444240d32f2f335a0779224fab26d
floridaalan <128455442+floridaalan@users.noreply.github.com>
2025-08-30 18:37:45 +0530

    Create Two_Sum_II_Input_Array_Is_Sorted.java

---
+++ Two_Sum_II_Input Array_Is_Sorted.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left=0;
+        
+        int right=numbers.length-1;
+        ArrayList <Integer> arr=new ArrayList<>();
+        int[] res=new int[2];
+        //pointers should not cross each other
+        while(left<right){
+            int sum=numbers[left]+numbers[right];
+            if(sum==target){
+                arr.add(left+1);
+                arr.add(right+1);
+                res[0]=arr.get(0);
+                res[1]=arr.get(1);
+                return res;
+            
+            }else if(sum<target){
+                left++;
+            }else if(sum>target){
+                right--;
+            }
+        }
+        return res;
+    }
+}
